08473c1435d7c68aa388b9b2f7abb8c2c2df3f7b
rahel <rahel.baumann1@students.unibe.ch>
2024-04-25 19:34:02 +0200

    Update reportingTool.py

--- reportingTool.py
+++ reportingTool.py
@@ -10,5 +10,12 @@ all_data_df = pd.read_csv('Daten/perronkante.csv', sep=';')
 pd.set_option('display.max_columns', None)
 print(all_data_df.head())
 
+data_types = all_data_df.dtypes
+print(data_types)
 
+# change object to string
+columns_to_string = ['Abkuerzung Bahnhof', 'Haltestellen Name', 'Perrontyp', 'Hilfstritt', 'Höhenverlauf', 'Material', 'Kantenart', 'Auftritt']
+all_data_df[columns_to_string] = all_data_df[columns_to_string].astype('string')
 
+# boolean spalte für Hilfstritt
+all_data_df['Hilfstritt boolean'] = np.where(all_data_df['Hilfstritt'] == 'vorhanden', True, False)
