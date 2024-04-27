24546d23dc6fcefb8d8742fa81e762ff06f5ff99
Shabani, Shaban (STUDENTS) <shaban.shabani@students.unibe.ch>
2024-04-27 22:12:53 +0200

    Check dtypes in columns by printing.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 def main():
-    df_perronkante = pd.read_csv('./Daten/perronkante.csv',sep=';')
+    # Read data from CSV File
+    df_perronkante = pd.read_csv('./Daten/perronkante.csv', sep=';')
+    
     print(df_perronkante)
-    print(df_perronkante.keys())
+    print(df_perronkante.dtypes)
 
 
 if __name__ == '__main__':
