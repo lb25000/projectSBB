72674cfc2aebdfe0be6c9cb79f88c25b3eda082d
Shabani, Shaban (STUDENTS) <shaban.shabani@students.unibe.ch>
2024-04-28 12:31:10 +0200

    Implemented user interaction with modules tkinter and pandastable.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from pandastable import Table
 
-def main():
+def readData():
     # Read data from CSV File
     df_perronkante = pd.read_csv('./Daten/perronkante.csv', sep=';')
-    print(df_perronkante['2_koord'])
 
     # Create DataFrame for coordinates
     koord = pd.DataFrame(data=df_perronkante['2_koord'].str.split(',', expand=True))
@@ -16,10 +19,40 @@ def main():
 
     # Remove redundant column "2_koord"
     df_perronkante = df_perronkante.drop("2_koord", axis='columns')
-    
-    # Printing to check the modifications
-    print(df_perronkante)
-    print(df_perronkante.dtypes)
+
+    return df_perronkante
+
+def showTable():
+    # Create a new Tkinter window
+    window = tk.Toplevel(root)
+    window.title("DataFrame Viewer")
+
+    # Create a Pandas Table object
+    table = Table(window, dataframe=df)
+    table.show()
+
+def plot_data():
+    # Add code to plot data here
+    messagebox.showinfo("Plot Data", "Plotting functionality will be added here.")
+
+def main():
+    global df
+    df = readData()
+
+    global root
+    root = tk.Tk()
+    root.title("SBB Dataset Exploration")
+
+    # Create a frame for the buttons
+    button_frame = ttk.Frame(root, padding="10")
+    button_frame.grid(column=0, row=0)
+
+    # Create buttons for different actions
+    ttk.Button(button_frame, text="Show Table", command=showTable).grid(column=0, row=0, padx=5, pady=5)
+    ttk.Button(button_frame, text="Plot data", command=plot_data).grid(column=0, row=2, padx=5, pady=5)
+    ttk.Button(button_frame, text="Exit", command=root.quit).grid(column=0, row=3, padx=5, pady=5)
+
+    root.mainloop()
 
 
 if __name__ == '__main__':
