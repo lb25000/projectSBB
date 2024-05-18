6cf583098b75877e5d70193bfa9148f923e84acc
Shabani, Shaban (STUDENTS) <shaban.shabani@students.unibe.ch>
2024-05-18 21:43:25 +0200

    Updated the main module.

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ This module initializes the application and starts the main loop.
 import tkinter as tk
 from table_gui import TableGUI
 
+def on_closing(root):
+    """
+    The program is exited if tkinter window will be closed.
+    """
+    root.destroy()
+    exit(0)
 
 def main():
     """
     Initialize the application and starts the main loop.
     """
     root = tk.Tk()
-    TableGUI(root) #pylint: unused variable, but the Gui display does not work without the app
+    app = TableGUI(root) #pylint: unused variable, but the Gui display does not work without the app
+    root.protocol("WM_DELETE_WINDOW", lambda: on_closing(root))
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
+    
