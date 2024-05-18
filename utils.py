0614314111c951845da87d85fc20e0becff5db15
Shabani, Shaban (STUDENTS) <shaban.shabani@students.unibe.ch>
2024-05-18 21:21:33 +0200

    Added docstring and fixed the code thanks pylint in utils.

--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
+"""
+Contains utility functions such as feedback window display.
+"""
 import tkinter as tk
 from tkinter import ttk
 
 def show_feedback_window(self, message):
-        """
-        opens a feedback window which shows the user what is incorrect about the input
-        :param message: indicates the type and nature of the error
-        """
-        feedback_window = tk.Toplevel(self.master)
-        feedback_window.title("Feedback")
-        feedback_label = ttk.Label(feedback_window, text=message)
-        feedback_label.pack(padx=10, pady=10)
+    """
+    opens a feedback window which shows the user what is incorrect about the input
+    :param message: indicates the type and nature of the error
+    """
+    feedback_window = tk.Toplevel(self.master)
+    feedback_window.title("Feedback")
+    feedback_label = ttk.Label(feedback_window, text=message)
+    feedback_label.pack(padx=10, pady=10)
